3d6895306dde93292ba42f7f05b4d0071257b19b
Jaeki-Kwon <worl1126@naver.com>
2021-05-25 16:17:17 +0900

    mongo db 암호 가리기

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// application/x-ww-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://Jacki-Kwon:jaeki@8840@signup.oqgl5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected!!"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("반갑다~!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 db에 넣어준다.
+  const user = new User(req.body);
+  // 몽고db 명령어
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
